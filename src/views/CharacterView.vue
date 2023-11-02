3a3719d07b8bb2764563586cfd2b48d54b5ac28f
evanlipp <lippevan@gmail.com>
2023-11-02 19:42:53 +0300

    update character page

--- src/views/CharacterView.vue
+++ src/views/CharacterView.vue
@@ -1,3 +1,25 @@
 <template>
-  <div>character page</div>
+  <div v-if="character">
+    <img :src="character.image" alt="character avatar" />
+    <p>{{ character.name }}</p>
+    <p>{{ character.species }}</p>
+    <p>{{ character.location.name }}</p>
+  </div>
 </template>
+
+<script setup>
+import axios from "axios";
+import { useRoute } from "vue-router";
+import { ref } from "vue";
+
+const route = useRoute();
+const character = ref();
+
+const fetchCharacter = async () => {
+  const response = await axios(
+    `https://rickandmortyapi.com/api/character/${route.params.id}`
+  );
+  character.value = response.data;
+};
+fetchCharacter();
+</script>
